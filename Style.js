522780856411e5b946441932ec0db9df71db63f9
Arvind-Adhithya <42761445+Arvind-Adhithya@users.noreply.github.com>
2019-07-02 19:56:39 +0530

    Create Style.js

---
+++ Style.js
@@ -0,0 +1,23 @@
+.card{
+width:20%;
+height:50%;
+background-color:green;
+text-align:center;
+margin: 15px 20px;
+padding: 5px;
+float:left;
+border-radius:5px;
+}
+.card:hover{
+transform:scale(0.9);
+}
+p{
+color:white;
+}
+p:hover { 
+  background-color: black;
+  //text-align:center;
+
+  width:200px;
+margin-left:30px;
+}
