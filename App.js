20eb4da0e9a6817c9c47cbff72d4f5a77dbb5dac
Arvind-Adhithya <42761445+Arvind-Adhithya@users.noreply.github.com>
2019-07-02 19:55:46 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,24 @@
+import React,{Component} from 'react';
+
+import Comp1 from './Comp1.js';
+
+//import Comp2 from './Comp2.js';
+import './Style.css';
+
+class App extends Component {
+render()
+ {
+return(<div ><Comp1 name ="Let us C" author ="NaRaYeN"/>
+<Comp1 name ="Deekir" author ="Deepak"/>
+<Comp1 name ="Cold" author ="Gowtham"/>
+<Comp1 name ="Let us C" author ="NaRaYeN"/>
+<Comp1 name ="Deekir" author ="Deepak"/>
+<Comp1 name ="Cold" author ="Gowtham"/>
+<Comp1 name ="Let us C" author ="NaRaYeN"/>
+<Comp1 name ="Deekir" author ="Deepak"/>
+<Comp1 name ="Cold" author ="Gowtham"/></div>)
+}
+}
+
+
+export default App;
